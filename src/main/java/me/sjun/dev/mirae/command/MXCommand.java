aaf6486aa22f03a8d6b38a8106dd543ae5f338a5
Min Jun Sung <themrsung@gmail.com>
2025-11-06 06:32:36 +0900

    First test

--- src/main/java/me/sjun/dev/mirae/command/MXCommand.java
+++ src/main/java/me/sjun/dev/mirae/command/MXCommand.java
@@ -1,5 +1,7 @@
 package me.sjun.dev.mirae.command;
 
+import me.sjun.dev.mirae.MiraeX;
+import me.sjun.dev.mirae.account.MXAccount;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.text.Component;
@@ -7,8 +9,12 @@ import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
 
 /**
  * MiraeX command.
@@ -84,6 +90,48 @@ public abstract class MXCommand extends Command {
         sender.sendMessage(Component.text(message).color(RED));
         sender.playSound(FAILURE_SOUND);
         return false;
+    }
+
+    /**
+     * Finds account by name exact.
+     *
+     * @param query The query
+     * @return The account if found
+     */
+    protected static @NotNull Optional<MXAccount> accountByNameExact(@Nullable String query) {
+        return Optional.ofNullable(query)
+                .flatMap(name -> MiraeX.getInstance().getAccountLedger().getAccounts()
+                        .stream()
+                        .filter(account -> account.getName().equalsIgnoreCase(name))
+                        .findAny());
+    }
+
+    /**
+     * Returns the list of accounts by name.
+     *
+     * @param query The name
+     * @return The list of accounts
+     */
+    protected static @NotNull List<MXAccount> accountsByName(@Nullable String query) {
+        if (query == null) return List.of();
+
+        String normalizedQuery = query.toLowerCase();
+        Map<String, MXAccount> accountMap = new TreeMap<>((a, b) -> {
+            if (a.equalsIgnoreCase(normalizedQuery)) return -1;
+            else if (b.equalsIgnoreCase(normalizedQuery)) return 1;
+            else if (a.startsWith(normalizedQuery)) return -1;
+            else if (b.startsWith(normalizedQuery)) return 1;
+            else if (a.contains(normalizedQuery)) return -1;
+            else if (b.contains(normalizedQuery)) return 1;
+
+            return 0;
+        });
+
+        MiraeX.getInstance().getAccountLedger().getAccounts().stream()
+                .filter(a -> a.getName().toLowerCase().contains(normalizedQuery))
+                .forEach(a -> accountMap.put(a.getName(), a));
+
+        return List.copyOf(accountMap.values());
     }
 
     /**
